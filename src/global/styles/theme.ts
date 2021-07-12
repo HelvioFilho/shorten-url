3f8deece58d398795dbe50c4141c118f46c474f9
Hélvio Filho <helviosvf@gmail.com>
2021-07-11 22:06:56 -0300

    Global styles

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  colors: {
+    white: '#FFFFFF',
+    blue100: '#132742',
+    blue20: '#1DDBB9',
+    blue40: '#2CCBB9',
+  },
+  fonts: {
+    title700: 'Roboto_700Bold',
+    text400: 'Roboto_400Regular',
+  }
+}
